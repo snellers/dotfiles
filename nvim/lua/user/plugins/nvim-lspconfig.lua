19ca50b694830f61480042a22c6d96634da78e5f
snellers <1669156+snellers@users.noreply.github.com>
2023-05-22 19:33:45 +0100

    lspconfig: use leader instead of space for some key binds.

--- nvim/lua/user/plugins/nvim-lspconfig.lua
+++ nvim/lua/user/plugins/nvim-lspconfig.lua
@@ -6,10 +6,10 @@ local servers = {
   'tsserver'
 }
 local opts = { noremap = true, silent = true }
-keymap('n', '<space>E', vim.diagnostic.open_float, opts)
+keymap('n', '<leader>E', vim.diagnostic.open_float, opts)
 keymap('n', '[d', vim.diagnostic.goto_prev, opts)
 keymap('n', ']d', vim.diagnostic.goto_next, opts)
-keymap('n', '<space>q', vim.diagnostic.setloclist, opts)
+keymap('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
@@ -20,16 +20,16 @@ local on_attach = function(client, bufnr)
   keymap('n', 'K', vim.lsp.buf.hover, bufopts)
   keymap('n', 'gi', vim.lsp.buf.implementation, bufopts)
   keymap('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  keymap('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  keymap('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  keymap('n', '<space>wl', function()
+  keymap('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  keymap('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  keymap('n', '<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  keymap('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  keymap('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  keymap('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  keymap('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+  keymap('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  keymap('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   keymap('n', 'gr', vim.lsp.buf.references, bufopts)
-  keymap('n', '<space>F', function()
+  keymap('n', '<leader>t', function()
     vim.lsp.buf.format({ async = true })
   end, bufopts)
 
