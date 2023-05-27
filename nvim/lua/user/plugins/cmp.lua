a05b16cc50e3e15d026fb6b3a4a32b2ccf0b800e
snellers <1669156+snellers@users.noreply.github.com>
2023-05-27 14:47:03 +0100

    remove tab bindings from cmp plugin

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -29,20 +29,6 @@ cmp.setup({
         fallback()
       end
     end, { 'i', 's' }),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lsp' },
