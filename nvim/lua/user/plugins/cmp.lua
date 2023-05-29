e31abc9b331b1c6bf33bdcfcc8289de9379890d8
snellers <1669156+snellers@users.noreply.github.com>
2023-05-29 12:54:13 +0100

    Bind tab in completion popup

--- nvim/lua/user/plugins/cmp.lua
+++ nvim/lua/user/plugins/cmp.lua
@@ -29,6 +29,20 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { "i" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i" }),
     ['<CR>'] = cmp.mapping(function(fallback)
       if not cmp.confirm(option) then
         fallback()
