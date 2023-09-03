2cf909a1910ef3b39a3c5ff2d7c9ed5db48cbaaa
snellers <1669156+snellers@users.noreply.github.com>
2023-09-03 16:29:31 +0100

    Add setup.sh

--- README.md
+++
@@ -1,8 +0,0 @@
-Some config settings for neovim, tmux and other tools.
-
-Requires unstable branch of neovim:
-
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt update
-sudo apt install neovim
-
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# A script for bootstrapping a fresh installation.
+set -euo pipefail
+cd
+echo "Installing some packages"
+sudo add-apt-repository ppa:neovim-ppa/neovim -y
+sudo apt install tmux powerline kitty neovim python3-dev python3-pip gparted
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
+
+echo "Setting up shell customizations"
+ln -sf ~/git/dotfiles/.tmux.conf.local
+ln -sf ~/git/dotfiles/.profile.local
+echo ". ~/.profile.local" >> .profile
+ln -sf ~/git/dotfiles/.bashrc.local
+echo ". ~/.bashrc.local" >> .bashrc
+cd .config
+if [ -d nvim ]; then
+    mv -f nvim nvim.old
+fi
+ln -sf ~/git/dotfiles/nvim 
+if [ -d kitty ]; then
+    mv -f kitty kitty.old
+fi
+cd
+
