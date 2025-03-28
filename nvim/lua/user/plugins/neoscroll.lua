7b20f5ce94db055371dd047c6df1e4df565334d4
snellers <1669156+snellers@users.noreply.github.com>
2025-03-28 10:50:08 +0000

    Stop using neoscroll set_mappings()

--- nvim/lua/user/plugins/neoscroll.lua
+++ nvim/lua/user/plugins/neoscroll.lua
@@ -1,7 +1,2 @@
 require('neoscroll').setup()
 
-require('neoscroll.config').set_mappings({
-  ['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '50' } },
-  ['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '50' } },
-})
-
